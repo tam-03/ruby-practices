fa5a3aa4080b808273588c1b032cd3d2f7a085fe
tam03 <tamuramura2803@gmail.com>
2020-12-21 13:52:11 +0900

    create ls_object

--- 08.ls_object/ls_object.rb
+++ 08.ls_object/ls_object.rb
@@ -1,15 +1,76 @@
 # frozen_string_literal: true
-
 require 'optparse'
 require 'etc'
 
-class FileDetails
-  def file_block(file_array)
-    puts "total #{file_array.map { |f| File.stat(f).blocks }.sum}"
+class Ls
+
+  def initialize(command)
+    @files = Ls.get_file(command)
   end
 
-  def file_type_to_s(f)
-    type = File.ftype(f)
+  def self.get_file(command)
+    file_array = Dir.entries('.').sort - Dir['.*']
+    file_array = Dir.entries('.').sort if command.a_option
+    file_array.reverse! if command.r_option
+    file_array
+  end
+
+  def file_detail
+    @files.map{ |file| FileDetail.new(file)}
+  end
+
+  def files_str_max_length
+    @files.map{ |file| FileDetail.new(file).file_length}.max
+  end
+
+  def block_total
+    @files.map { |file| FileDetail.new(file).block }.sum
+  end
+
+end
+
+class Print
+  def initialize(files)
+    @files = files
+  end
+
+  def horizontal
+    str_max_length = @files.files_str_max_length + 2
+    (0..6).map { |num|
+      lines = []
+      @files.file_detail.each_slice(7) { |files|
+        lines << files[num]
+      }
+      lines.map { |file|
+        print file.to_s.ljust(str_max_length)
+      }
+      puts "\n"
+    }
+  end
+
+  def list
+    puts " total #{@files.block_total}"
+    @files.file_detail.each { |file|
+      print file.type
+      print file.parmission
+      print "#{file.link} "
+      print "#{file.uid_name} "
+      print "#{file.gid_name} "
+      print "#{file.size.to_s.rjust(5)} "
+      print file.update_day
+      puts file.to_s
+    }
+  end
+
+end
+
+class FileDetail
+  def initialize(file)
+    @file = file
+  end
+
+  def type
+    type = File.ftype(@file)
     {
       "file": '-',
       "directory": 'd',
@@ -17,8 +78,8 @@ class FileDetails
     }[:"#{type}"]
   end
 
-  def file_parmission(f)
-    mode = File.stat(f).world_readable?
+  def parmission
+    mode = File.stat(@file).world_readable?
     parmission = format('%o', mode)
     pms = parmission.to_s.split(//).to_a
     (0..2).each do |i|
@@ -35,85 +96,63 @@ class FileDetails
     end
   end
 
-  def file_link(f)
-    file_link = File.stat(f).nlink.to_s
-    ' ' + file_link.rjust(3).to_s + ' '
+  def link
+    file_link = File.stat(@file).nlink.to_s
+    file_link.rjust(3).to_s
   end
 
-  def file_uid_name(f)
-    Etc.getpwuid(File.stat(f).uid).name
+  def uid_name
+    Etc.getpwuid(File.stat(@file).uid).name
   end
 
-  def file_gid_name(f)
-    Etc.getgrgid(File.stat(f).gid).name
+  def gid_name
+    Etc.getgrgid(File.stat(@file).gid).name
   end
 
-  def file_size(f)
-    file_size = File.stat(f).size
-    file_size.to_s.rjust(5)
+  def size
+    file_size = File.stat(@file).size
   end
 
-  def file_update_day(f)
-    mtime = File.stat(f).mtime
+  def update_day
+    mtime = File.stat(@file).mtime
     mtime_a = mtime.to_a
     mtime_s = mtime.strftime('%R')
     (mtime_a[4].to_s.rjust(2) + mtime_a[3].to_s.rjust(3)).to_s + ' ' + mtime_s + ' '
+    # monthを文字列に変更
+  end
+
+  def file_length
+    @file.size
+  end
+
+  def block
+    File.stat(@file).blocks
+  end
+
+  def to_s
+    @file
   end
 end
 
-class Ls
-  def l_option(file_array)
-    l_files = FileDetails.new
-    l_files.file_block(file_array)
-    file_array.map do |f|
-      l_files = FileDetails.new
-      print l_files.file_type_to_s(f)
-      print l_files.file_parmission(f)
-      print l_files.file_link(f)
-      print l_files.file_uid_name(f) + ' '
-      print l_files.file_gid_name(f) + ' '
-      print l_files.file_size(f) + ' '
-      print l_files.file_update_day(f)
-      puts f
-    end
+class Command
+
+  def initialize(command = ARGV.getopts('arl'))
+    @command = command
+  end
+
+  def a_option
+   return @command["a"]
+  end
+
+  def r_option
+   return @command["r"]
+  end
+
+  def l_option
+   return @command["l"]
   end
 end
 
-class Print < Ls
-  def print_row(file_array)
-    str_max_length = file_array.map(&:size).max + 2
-    (0..6).map do |num|
-      lines = []
-      file_array.each_slice(7) do |files|
-        lines << files[num]
-      end
-      lines.map do |file|
-        if file.nil?
-          print ' '
-        else
-          print file.ljust(str_max_length)
-        end
-      end
-      puts "\n"
-    end
-  end
-end
-
-class Command < Ls
-  def select_option
-    horizontal = Print.new
-    list = Ls.new
-    files = ARGV.getopts('arl')
-    file = Dir.entries('.').sort - Dir['.*']
-    file = Dir.entries('.').sort if files['a']
-    file.reverse! if files['r']
-    if files['l']
-      list.l_option(file)
-    else
-      horizontal.print_row(file)
-    end
-  end
-end
-
-file = Command.new
-file.select_option
+command = Command.new
+files = Ls.new(command)
+command.l_option ? Print.new(files).list : Print.new(files).horizontal
